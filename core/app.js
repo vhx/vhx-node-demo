d69dc8e72793db4e6aa5b4839819b8ff3f69f72b
Scott Robertson <scott@scottusrobus.com>
2016-02-12 11:26:59 -0800

    clean up app.js

--- core/app.js
+++ core/app.js
@@ -11,8 +11,7 @@ const session     = require('express-session');
 const cookie      = require('cookie-parser');
 const parser      = require('body-parser');
 const uuid        = require('uuid');
-const path        = require('path');
-const template    = require('./views/templates');
+const template    = require('../views/templates');
 const vhx         = require('vhx')(process.env.VHX_API_KEY);
 const app         = express();
 
@@ -20,7 +19,7 @@ const app         = express();
 ..............................*/
 app.set('port', process.env.PORT);
 app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
 
@@ -45,10 +44,13 @@ app.get('/', function(req, res) {
   a call to vhx.collections to get back our collection items
   http://dev.vhx.tv/docs/api/?javascript#collection-items-list
   ..................................... */
+
+  // 3 collections each with 4 items
   vhx.collections.listItems({
-    collection: process.env.VHX_COLLECTION_ID
+    collection: 'https://api.vhx.tv/collections/1896',
+    product: 'https://api.vhx.tv/products/14444'
   }, function(err, collections) {
-    template(res, {
+    template(req, res, {
       layout: 'layout',
       yield: 'home/home',
       data: {
@@ -70,55 +72,52 @@ app.post('/join', function(req, res) {
     vhx.customers.create({
       name: req.body.customer.name,
       email: req.body.customer.email,
-      product: process.env.VHX_PRODUCT_ID
+      product: 'https://api.vhx.tv/products/14444'
     }, function(err, customer) {
-      req.session.customer_id = customer.id;
+      req.session.customer_href = 'https://api.vhx.tv/customers/' + customer.id;
       res.send('well done');
     });
   }
 });
 
 app.post('/login', function(req, res) {
-  req.session.customer_id = 2041092;
-  res.redirect('/watch/54355');
+
   /* User Account Authorization
   .....................................
-  For the sake of this demo site, no actually user authorization
-  is implemented. You can authorize your users how ever you want.
-  We recommend http://passportjs.org/. On POST to this route (/login),
-  we'll mock login the mock user by created a session with the
-  customer id.
+  For the sake of this demo site, no actual user authorization
+  is implemented. You can authorize your users however you want.
+  We recommend http://passportjs.org/.
+
+  On POST to this route (/login), we mock login the user
+  by creating a session with the customer vhx href. The customer href
+  would be something you store in your own DB on account creation.
   ..................................... */
-
-
-  // vhx.customers.retrieve(15, function(err, customer) {
-  //   // asynchronously called
-  // });
-  // req.session.customer_id
-  //
-  // template(res, {
-  //   layout: 'layout',
-  //   yield: 'modals/login'
-  // });
+  req.session.customer_href = 'https://api.vhx.tv/customers/2041092';
+  req.session.customer_email = req.body.customer.email;
+  
+  let url = req.query.redirect ? req.query.redirect : '/';
+  res.redirect(url);
 });
 
 app.get('/login', function(req, res) {
-  template(res, {
+  template(req, res, {
     layout: 'layout',
     yield: 'modals/login'
   });
 });
 
 app.get('/watch/:video_id', function(req, res) {
-  console.log(req.session.customer_id);
 
   if (!req.params.video_id) {
     res.send('Not Found');
     return;
   }
-  
-  if (!req.session.customer_id) {
-    res.send('Not Authorized');
+
+  if (!req.session.customer_href) {
+    template(req, res, {
+      layout: 'layout',
+      yield: 'watch/unauthorized'
+    });
     return;
   }
 
@@ -128,15 +127,19 @@ app.get('/watch/:video_id', function(req, res) {
   http://dev.vhx.tv/docs/api/?javascript#authorizations-create
   ..................................... */
   vhx.authorizations.create({
-    customer: req.session.customer_id,
-    video: req.params.video_id
+    customer: req.session.customer_href,
+    video: 'https://api.vhx.tv/videos/' + req.params.video_id
   }, function(err, authorization) {
     if (err) {
-      res.send('Not Authorized');
-    } else {
-      template(res, {
+      template(req, res, {
         layout: 'layout',
-        yield: 'watch/watch',
+        yield: 'watch/unauthorized'
+      });
+    } else {
+      console.log(authorization);
+      template(req, res, {
+        layout: 'layout',
+        yield: 'watch/player',
         data: {
           authorization: authorization
         }
