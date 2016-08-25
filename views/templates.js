8e91c0967ae9e8dd25a5f4fa371197cec9cafe04
Scott Robertson <scott@scottusrobus.com>
2016-08-25 09:39:40 -0700

    add partial template method

--- views/templates.js
+++ views/templates.js
@@ -30,13 +30,13 @@ walk.walkSync(views_dir, function(base_dir, filename) {
 .................................*/
 const getTemplateHelperData = function(req, obj) {
   return {
-    yield: obj.yield,
+    yield: obj.yield ? obj.yield : '',
     random_email: 'vhx.demo+' + random.generate(12) +  '@gmail.com',
     logged_in: (req.session.customer_href) ? true : false,
     current_path: req.path,
-    hasBack: (obj.yield.indexOf('watch') >= 0) ? true : false,
-    hasFooter: (!obj.yield.match(/unauthorized|modals/)) ? true : false,
-    hasModals: (obj.yield === 'home/home') ? true : false,
+    hasBack: (obj.yield && obj.yield.indexOf('watch') >= 0) ? true : false,
+    hasFooter: (obj.yield && !obj.yield.match(/unauthorized|modals/)) ? true : false,
+    hasModals: (obj.yield && obj.yield === 'home/home') ? true : false,
     customer: {
       email: req.session.customer_email
     },
@@ -49,10 +49,20 @@ const getTemplateHelperData = function(req, obj) {
 /* Template Rendering
 ..............................*/
 const Template = function(req, res, obj) {
-  let layout = handlebars.compile(fs.readFileSync(views_dir + '/layouts/' + obj.layout + '.hbs').toString());
   let data = _.extend({}, getTemplateHelperData(req, obj), obj.data);
 
-  res.send(layout(data));
+  if (obj.layout) {
+    let layout = handlebars.compile(fs.readFileSync(views_dir + '/layouts/' + obj.layout + '.hbs').toString());
+    return res.send(layout(data));
+  }
+  if (obj.partial) {
+    let partial = handlebars.compile(fs.readFileSync(views_dir + '/' + obj.partial + '.hbs').toString());
+    return res.json({
+      id: req.query.id,
+      partial: partial(data)
+    });
+  }
+
 };
 
 
