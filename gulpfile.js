10cdfbf5216a65e7ce5f4d1be68a6d063783a937
Hannah Nordgren <hey@hereshannahs.info>
2017-08-21 15:07:36 -0400

    [FEATURE] got testing environment set up with documentation

--- gulpfile.js
+++ gulpfile.js
@@ -3,29 +3,17 @@ var sass = require('gulp-sass');
 var autoprefixer = require('autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
 var postcss      = require('gulp-postcss');
-const browserSync = require('browser-sync').create();
-
-gulp.task('browserSync', function() {
-  browserSync.init({
-    server: {
-      baseDir: 'app'
-    },
-  })
-})
 
 
 // watches current scss files ONLY.
 gulp.task('sass', function(){
-	return gulp.src('./styles/partials/*.scss')
+	return gulp.src('./styles/**/*.scss')
 		.pipe(sass())
 		.pipe(gulp.dest('./public'))
-		.pipe(browserSync.reload({
-			stream: true
-		}))
 });
 
 gulp.task('autoprefixer', function () {
-  return gulp.src('./public/index.css')
+  return gulp.src('public/index.css')
     .pipe(sourcemaps.init())
     .pipe(postcss([ autoprefixer() ]))
     .pipe(sourcemaps.write('.'))
@@ -36,5 +24,4 @@ gulp.task('autoprefixer', function () {
 gulp.task('watch', ['sass', 'autoprefixer'], function(){
 	gulp.watch('./styles/partials/*.scss', ['sass']);
 	gulp.watch('./public/index.css', ['autoprefixer']);
-	gulp.watch('views/layouts/layout.hbs', browserSync.reload);
 });
